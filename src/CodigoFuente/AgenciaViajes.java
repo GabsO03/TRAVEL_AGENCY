15c2e1ff2fec06b98877e809f7c2cfead2c77fa5
GabsO03 <gabrielaoria66@gmail.com>
2023-11-07 10:26:24 +0100

    codigo

---
+++ src/CodigoFuente/AgenciaViajes.java
@@ -0,0 +1,9 @@
+package CodigoFuente;
+
+import java.util.Scanner;
+
+public class AgenciaViajes {
+    public static void main (String[]args) {
+        Scanner escaner = new Scanner(System.in);
+    }
+}
