f6c5908217ab686a914ef1d95250949ef5fdbb0e
GabsO03 <gabrielaoria66@gmail.com>
2023-11-07 21:50:31 +0100

    update case 2

--- src/CodigoFuente/AgenciaViajes.java
+++ src/CodigoFuente/AgenciaViajes.java
@@ -6,7 +6,7 @@ public class AgenciaViajes {
     public static void main(String[] args) {
         Scanner escaner = new Scanner(System.in);
         int opcionprograma, numeroViajeros = 5;
-        float gastosViajeros = 0, gastoTotal = 0, reparto = 0; //Variables para Gabi
+        float gastosViajeros = 0, gastoTotal = 0, reparto = 0, v1=0, v2=0, v3=0, v4=0, v5=0; //Variables para Gabi
         boolean opcionuno = false, opciondos = false; //booleanas para avisar que los pasos se han cumplido.
 
             do {
@@ -23,12 +23,12 @@ public class AgenciaViajes {
 
                     }
                     case 2: {
-                        if (opcionuno) { //Introduce aqui el código Gabi.
+                        if (opcionuno) {
+                            gastoTotal=(v1+v2+v3+v4+v5)/numeroViajeros;
+                            System.out.println();
                             opciondos = true;
                         } else System.out.println("No has completado la opcion 1");
-
                         break;
-
                     }
                     case 3: { //Introduce el código aqui David.
 
