8000bfbea42d360a12f45c51ac9a9081358cc27b
jesus <jmoral.jj@gmail.com>
2023-11-07 20:40:24 +0100

    añado la primera opcion y un retoque del primero bucle para que pida hasta que las opciones sean correctas

--- src/CodigoFuente/AgenciaViajes.java
+++ src/CodigoFuente/AgenciaViajes.java
@@ -8,37 +8,47 @@ public class AgenciaViajes {
         int opcionprograma, numeroViajeros = 5;
         float gastosViajeros = 0, gastoTotal = 0, reparto = 0; //Variables para Gabi
         boolean opcionuno = false, opciondos = false; //booleanas para avisar que los pasos se han cumplido.
-
-            do {
-                System.out.println("Selecione la opcion\n" +
-                        "1. Introducir los gastos de los viajeros\n" +
-                        "2. Calcular y mostrar repartos de gastos\n" +
-                        "3. Salir");
-                opcionprograma = escaner.nextInt();
-                switch (opcionprograma) {
-                    case 1: {  //Introduce el código aqui Jesus.
-
-                        opcionuno = true;
-                        break;
-
-                    }
-                    case 2: {
-                        if (opcionuno) { //Introduce aqui el código Gabi.
-                            opciondos = true;
-                        } else System.out.println("No has completado la opcion 1");
-
-                        break;
-
-                    }
-                    case 3: { //Introduce el código aqui David.
-
-                    }
+        do {
+            System.out.println("Selecione la opcion\n" +
+                    "1. Introducir los gastos de los viajeros\n" +
+                    "2. Calcular y mostrar repartos de gastos\n" +
+                    "3. Salir");
+            opcionprograma = escaner.nextInt();
+            switch (opcionprograma) {
+                case 1: {  //Introduce el código aqui Jesus.
+                    System.out.println("ingrese los gastos de los viajeros si no tienen gastos ingresear un -1");
+                    System.out.println("gastos del primero viajero");
+                    gastosViajeros = 5;
+                    int viajero = escaner.nextInt();
+                    System.out.println("gastos del segundo viajero");
+                    int viajero2 = escaner.nextInt();
+                    if (viajero2 == -1) gastosViajeros = gastosViajeros - 1;
+                    System.out.println("gastos del tercer viajero");
+                    int viajero3 = escaner.nextInt();
+                    if (viajero3 == -1) gastosViajeros = gastosViajeros - 1;
+                    System.out.println("gastos del cuarto viajero");
+                    int viajero4 = escaner.nextInt();
+                    if (viajero4 == -1) gastosViajeros = gastosViajeros - 1;
+                    System.out.println("gastos del quinto viajero");
+                    int viajero5 = escaner.nextInt();
+                    if (viajero5 == -1) gastosViajeros = gastosViajeros - 1;
+                    if (gastosViajeros == 1) System.out.println("debes de introducir mas viajeros");
+                    opcionuno = true;
+                    break;
                 }
-            }while (opcionprograma != 3) ;
+                case 2: {
+                    if (opcionuno) { //Introduce aqui el código Gabi.
+                        opciondos = true;
+                    } else System.out.println("No has completado la opcion 1");
 
+                    break;
 
-        }
+                }
+                case 3: { //Introduce el código aqui David.
+
+                }
+            }
+        }while(opcionprograma>3);
     }
-
-
+}
 
