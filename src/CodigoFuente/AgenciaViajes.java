21e861fa216965b41f7bbe57b8fcd45670d573cf
Alejandro Ordoñez Pegalajar <alejandroordonezp@gmail.com>
2023-11-07 17:26:25 +0100

    He  modificado las variables y he creado un switch para cada una de las opciones: Recordar que la opción 2 incluye el punto 2 y 3 del pdf, También añadi comentarios para que no os perdáis. Cualquier cosa MD A MI ;)

--- src/CodigoFuente/AgenciaViajes.java
+++ src/CodigoFuente/AgenciaViajes.java
@@ -5,44 +5,40 @@ import java.util.Scanner;
 public class AgenciaViajes {
     public static void main(String[] args) {
         Scanner escaner = new Scanner(System.in);
-        int opcionprograma, numeroViajeros=0;
-        float gastosViajeros = 0, gastoTotal =0, reparto = gastoTotal/ numeroViajeros;
-        System.out.println("Introduce el número de viajeros");
-        numeroViajeros = Integer.parseInt(escaner.nextLine());
-        if (numeroViajeros<1 || numeroViajeros>5){
-            System.out.println("Valor no valido, un viaje debe tener de 1 a 5 viajeros");
-        } else {
+        int opcionprograma, numeroViajeros = 5;
+        float gastosViajeros = 0, gastoTotal = 0, reparto = 0; //Variables para Gabi
+        boolean opcionuno = false, opciondos = false; //booleanas para avisar que los pasos se han cumplido.
+
             do {
                 System.out.println("Selecione la opcion\n" +
                         "1. Introducir los gastos de los viajeros\n" +
                         "2. Calcular y mostrar repartos de gastos\n" +
                         "3. Salir");
                 opcionprograma = escaner.nextInt();
-            } while (opcionprograma > 3);
-            do {
-                if (opcionprograma == 1) {
-                    System.out.println("ingrese los gastos de los viajeros si no tienen gastos ingresear un -1");
-                    System.out.println("gastos del primero viajero");
-                    gastosViajeros = 5;
-                    int viajero = escaner.nextInt();
-                    System.out.println("gastos del segundo viajero");
-                    int viajero2 = escaner.nextInt();
-                    if (viajero2 == -1) gastosViajeros = gastosViajeros - 1;
-                    System.out.println("gastos del tercer viajero");
-                    int viajero3 = escaner.nextInt();
-                    if (viajero3 == -1) gastosViajeros = gastosViajeros - 1;
-                    System.out.println("gastos del cuarto viajero");
-                    int viajero4 = escaner.nextInt();
-                    if (viajero4 == -1) gastosViajeros = gastosViajeros - 1;
-                    System.out.println("gastos del quinto viajero");
-                    int viajero5 = escaner.nextInt();
-                    if (viajero5 == -1) gastosViajeros = gastosViajeros - 1;
-                    if (gastosViajeros == 1) System.out.println("debes de introducir mas viajeros");
+                switch (opcionprograma) {
+                    case 1: {  //Introduce el código aqui Jesus.
+
+                        opcionuno = true;
+                        break;
+
+                    }
+                    case 2: {
+                        if (opcionuno) { //Introduce aqui el código Gabi.
+                            opciondos = true;
+                        } else System.out.println("No has completado la opcion 1");
+
+                        break;
+
+                    }
+                    case 3: { //Introduce el código aqui David.
+
+                    }
                 }
+            }while (opcionprograma != 3) ;
 
 
-            } while (gastosViajeros == 1);
         }
     }
-}
+
+
 
